cc51d976a39e6b39291b54321f90788ada100d68
AHEKDOT <vadim.plehanow@mail.ru>
2024-08-05 14:47:52 +0500

    Добавлено отображение причины ошибки

--- src/ru/practicum/sprint4/exceptions/ManagerSaveException.java
+++ src/ru/practicum/sprint4/exceptions/ManagerSaveException.java
@@ -5,4 +5,11 @@ public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message) {
         super(message);
     }
+
+    public ManagerSaveException(String message, Exception e) {
+        super(message);
+        if (e.getCause() != null) {
+            System.out.println("Причина: " + e.getCause());
+        }
+    }
 }
