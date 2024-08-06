ec04faf412fc1d257d9322a772e3a4709b88fc90
AHEKDOT <vadim.plehanow@mail.ru>
2024-08-06 12:13:41 +0500

    убрана проверка на null

--- src/ru/practicum/sprint4/exceptions/ManagerSaveException.java
+++ src/ru/practicum/sprint4/exceptions/ManagerSaveException.java
@@ -8,8 +8,6 @@ public class ManagerSaveException extends RuntimeException {
 
     public ManagerSaveException(String message, Exception e) {
         super(message);
-        if (e.getCause() != null) {
-            System.out.println("Причина: " + e.getCause());
-        }
+        System.out.println("Причина: " + e.getCause());
     }
 }
